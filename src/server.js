549faaab63db8eda93896b31370477ecd2dcda7c
nischal <nischalmadikiri10@gmail.com>
2023-11-08 23:32:04 +0530

    using http-status-codes npm package to make status codes more readable

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 
+const StatusCodes = require("http-status-codes");
 const express = require("express");
 const app = express();
 
-app.get("/hello", (req, res) => res.send("Hello, Node!!!"));
+app.get("/hello", (req, res) => {
+    res.status(StatusCodes.CREATED),
+    res.send("Hello, Node!!!");
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Hey! The server is running at localhost:${process.env.PORT}`);
+  console.log(`Hey! The server is running at localhost:${process.env.PORT}`);
 });
